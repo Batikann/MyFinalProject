ae07906b37b08d96461cecb42215f0a3db32d886
Batikann <emiruar123@gmail.com>
2021-08-03 10:56:09 +0300

    add WebAPI layer and configurations

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -31,7 +31,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 12)
+            if (DateTime.Now.Hour == 17)
             {
                 return new ErrorDataResult<List<Product>>(Messages.SystemOff);
             }
