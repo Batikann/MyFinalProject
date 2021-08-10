d1ae0d1542d4f460ee13eea05ec65dca14bc12f9
Batikann <emiruar123@gmail.com>
2021-08-10 15:27:38 +0300

    JWT Configurations Commit 2

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -12,5 +13,6 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError= "The number of products that can be added to this Category has been exceeded. ";
         public static string ProductNameUsed = "Product Name Previously Used";
         public static string CategoryLimitExceded= "Unable to add new product because category limit has been exceeded.";
+        internal static string AuthorizationDenied= "you are not authorized ";
     }
 }
